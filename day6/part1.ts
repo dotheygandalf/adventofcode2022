cef1799eeb126dd2267514311c9adba2ff1858d7
Jonathan Chang <jonathanchang@netflix.com>
2022-12-10 17:24:37 -0800

    use enum to switch between part 1 and 2

--- day6/part1.ts
+++ day6/part1.ts
@@ -5,17 +5,20 @@ const data = getDataFromFile(path.resolve(__dirname, './input.txt'));
 // const data = getDataFromFile(path.resolve(__dirname, './input-test.txt'));
 const lines = splitLines(data);
 
+// const MARKER_LENGTH = 4;
+const MARKER_LENGTH = 14;
+
 const findMarker = (input: string): number => {
   let marker = '';
   console.log(input);
-  for (let i = 1; i < input.length - 4; i++) {
-    let currentCandidate = input.substring(i, i + 4);
+  for (let i = 1; i < input.length - MARKER_LENGTH; i++) {
+    let currentCandidate = input.substring(i, i + MARKER_LENGTH);
     for (const [index, char] of currentCandidate.split('').entries()) {
       if (
         Array.from(currentCandidate.matchAll(new RegExp(char, 'g'))).length > 1
       ) {
         break;
-      } else if (index === 3) {
+      } else if (index === MARKER_LENGTH - 1) {
         marker = currentCandidate;
       }
     }
@@ -24,7 +27,7 @@ const findMarker = (input: string): number => {
     }
   }
   console.log(marker);
-  return input.indexOf(marker) + 4;
+  return input.indexOf(marker) + MARKER_LENGTH;
 };
 
 const parseLines = (lines: string[]) => {
